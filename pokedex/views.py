676bcecf524f4abb2ddf107887ab8731f78b35cc
gabriel <gabriel.filiot@epsi.fr>
2021-12-08 17:04:00 +0100

    recherche par id dans l'api

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,9 +1,10 @@
 import requests
 from django.shortcuts import render
 
-def index(request):
-    url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+def index(request, nb):
+    url = "https://pokeapi.co/api/v2/pokemon-species/" + str(nb)
     response = requests.get(url)
     pokemon = response.json()
+    parsejson(pokemon)
 
     return render(request, 'pokedex/index.html', {'poke':pokemon})
