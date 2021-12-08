6074e3efbb8f0fb7e2e1bb0ce49b35cc601e9a16
gabriel <gabriel.filiot@epsi.fr>
2021-12-08 17:12:24 +0100

    affichage nom pokemon fr

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,10 +1,12 @@
 import requests
+import json
 from django.shortcuts import render
 
 def index(request, nb):
     url = "https://pokeapi.co/api/v2/pokemon-species/" + str(nb)
     response = requests.get(url)
-    pokemon = response.json()
-    parsejson(pokemon)
+    pokemon = response.text
+    parse_json = json.loads(pokemon)
+    name = parse_json["names"][4]["name"]
 
-    return render(request, 'pokedex/index.html', {'poke':pokemon})
+    return render(request, 'pokedex/index.html', {'name':name})
