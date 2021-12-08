01a264daa01e299d2377ab856b166b838e540369
gabriel <gabriel.filiot@epsi.fr>
2021-12-08 16:43:47 +0100

    template + vue

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,3 +1,9 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+    response = requests.get(url)
+    pokemon = response.json()
+
+    return render(request, 'pokedex/index.html', {'poke':pokemon})
