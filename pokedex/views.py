47a2a51cfd62f6b6eed2aa4710a876d86ccb0dd8
gabgabb <70889534+gabgabb@users.noreply.github.com>
2022-01-06 00:02:31 +0100

    optimisation de la requête allPokemon

--- pokedex/views.py
+++ pokedex/views.py
@@ -5,8 +5,6 @@ import logging
 from django.shortcuts import render
 from .forms import PokemonForm
 
-
-
 def index(request, nb):
     url = "https://pokeapi.co/api/v2/pokemon-species/" + str(nb)
     urlPokemon = "https://pokeapi.co/api/v2/pokemon/" + str(nb)
@@ -20,17 +18,17 @@ def index(request, nb):
     pokemon = response.text
     parse_json = json.loads(pokemon)
     name = parse_json["names"][4]["name"]
-    
+
     return render(request, 'pokedex/index.html', {'name': name, 'sprite': spriteUrl})
+
 
 def pageAcceuil(request, offset=0, limit=30):
     urlAllPokemon = "https://pokeapi.co/api/v2/pokemon/?offset=" + str(offset) + "&limit=" + str(limit)
-
+    responseAllPokemon = requests.get(urlAllPokemon).text
+    parse_AllPokemon = json.loads(responseAllPokemon)
     data = []
 
-    for i in range(limit):
-        responseAllPokemon = requests.get(urlAllPokemon).text
-        parse_AllPokemon = json.loads(responseAllPokemon)
+    for i in range(limit - offset):
         url_ALl = parse_AllPokemon["results"][i]["url"]
 
         responsePokemon = requests.get(url_ALl).text
@@ -40,9 +38,10 @@ def pageAcceuil(request, offset=0, limit=30):
 
         data.append({"id": i + offset + 1, "name": name, "sprite": spriteUrl})
 
-    return render(request, 'pokedex/acceuil.html', {'data': data})
-    
-def src_pokemon(request):    
+    return render(request, 'pokedex/acceuil.html', {'data': data, 'offset': offset, 'limit': limit})
+
+
+def src_pokemon(request):
     if request.method == 'POST':
         form = PokemonForm(request.POST)
         if form.is_valid():
@@ -55,4 +54,3 @@ def src_pokemon(request):
             return HttpResponseRedirect(str(id))
     else:
         form = PokemonForm()
- 
