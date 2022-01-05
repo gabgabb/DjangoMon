2880f0918e0e826222f2d51e7c359abce9f8057f
Antoine <a.deyris85@gmail.com>
2022-01-05 12:23:04 +0100

    FIX : virgule path

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('<int:nb>', views.index, name="index"),
     path('acceuil/', views.pageAcceuil),
-    path('acceuil/<int:offset>/<int:limit>', views.pageAcceuil, name="acceuil")
+    path('acceuil/<int:offset>/<int:limit>', views.pageAcceuil, name="acceuil"),
     path('src_pokemon', views.src_pokemon, name="src_pokemon"),
     #path('bellebite/', views.hello, name="bite"),
 ]
