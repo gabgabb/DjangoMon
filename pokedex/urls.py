2f3382b5ff1ab186163ddf7c0b3263cd06793d8b
Antoine <a.deyris85@gmail.com>
2022-01-06 16:39:25 +0100

    FIX : searchbar in accueil (unless... ?)

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('<int:nb>', views.index, name="index"),
-    path('accueil/', views.pageAccueil),
-    path('accueil/<int:offset>/<int:limit>', views.pageAccueil, name="accueil"),
+    path('', views.pageAccueil),
+    path('<int:offset>/<int:limit>', views.pageAccueil, name="accueil"),
     path('src_pokemon', views.src_pokemon, name="src_pokemon"),
     #path('bellebite/', views.hello, name="bite"),
 ]
