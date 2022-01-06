fc3b31ad430dcf94e71b88330642bab9c3a7c3f2
gabgabb <70889534+gabgabb@users.noreply.github.com>
2022-01-06 17:10:30 +0100

    fix search (vraiment)

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('<int:nb>', views.index, name="index"),
-    path('', views.pageAccueil),
-    path('<int:offset>/<int:limit>', views.pageAccueil, name="accueil"),
+    path('accueil/', views.pageAccueil),
+    path('accueil/<int:offset>/<int:limit>', views.pageAccueil, name="accueil"),
     path('src_pokemon', views.src_pokemon, name="src_pokemon"),
-    path('acceuil/src_pokemon', views.src_pokemon, name="src_pokemon"),
+    path('accueil/src_pokemon', views.src_pokemon, name="src_pokemon"),
 ]
