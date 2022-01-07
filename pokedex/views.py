300e774c8cdb2791eab399adb0a5a2e533cf58f7
gabgabb <70889534+gabgabb@users.noreply.github.com>
2022-01-07 18:32:21 +0100

    async accueil

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,9 +1,15 @@
-from django.http.response import HttpResponseRedirect
+import random
+import time
 import requests
 import json
+import aiohttp
+import asyncio
+
 import logging
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from .forms import PokemonForm
+
 
 def index(request, nb):
     url = "https://pokeapi.co/api/v2/pokemon-species/" + str(nb)
@@ -22,23 +28,53 @@ def index(request, nb):
     return render(request, 'pokedex/index.html', {'name': name, 'sprite': spriteUrl})
 
 
-def pageAcceuil(request, offset=0, limit=30):
+async def pageAccueil(request, offset=0, limit=30):
+    start_time = time.time()  # timer
     urlAllPokemon = "https://pokeapi.co/api/v2/pokemon/?offset=" + str(offset) + "&limit=" + str(limit)
     responseAllPokemon = requests.get(urlAllPokemon).text
     parse_AllPokemon = json.loads(responseAllPokemon)
-    data = []
 
-    for i in range(limit - offset):
-        url_ALl = parse_AllPokemon["results"][i]["url"]
+    urlSpecies = "https://pokeapi.co/api/v2/pokemon-species/?offset=" + str(offset) + "&limit=" + str(limit)
+    species1 = requests.get(urlSpecies).text
+    species2 = json.loads(species1)
 
-        responsePokemon = requests.get(url_ALl).text
-        parse_Pokemon = json.loads(responsePokemon)
-        name = parse_AllPokemon["results"][i]["name"]
-        spriteUrl = parse_Pokemon["sprites"]["other"]["official-artwork"]["front_default"]
+    actionsAllPokemon = []
+    actionsAllSpecies = []
+    dataRender = []
 
-        data.append({"id": i + offset + 1, "name": name, "sprite": spriteUrl})
+    if limit >= 30 or offset < limit:
+        async with aiohttp.ClientSession() as session:
+            for i in range(limit - offset):
+                url_ALl = parse_AllPokemon["results"][i]["url"]
+                url_all_species = species2["results"][i]["url"]
 
-    return render(request, 'pokedex/acceuil.html', {'data': data, 'offset': offset, 'limit': limit})
+                actionsAllPokemon.append(asyncio.ensure_future(getPokemonData(session, url_ALl)))
+                actionsAllSpecies.append(asyncio.ensure_future(getPokemonData(session, url_all_species)))
+
+            print("--- %s seconds gather0 ---" % (time.time() - start_time))
+            result = await asyncio.gather(*actionsAllPokemon)
+            print("--- %s seconds gather1 ---" % (time.time() - start_time))
+            result2 = await asyncio.gather(*actionsAllSpecies)
+            print("--- %s seconds gather2 ---" % (time.time() - start_time))
+
+            for i, pokemonList in enumerate(result):
+                pokemonJson = json.loads(json.dumps(pokemonList))
+                nameFrench = json.loads(json.dumps(result2[i]))["names"][4]["name"]
+                # pokemonSpeciesList = await asyncio.gather(getPokemonData(session, pokemonJson["species"]["url"]))
+                # nameFrench = json.loads(requests.get(pokemonJson["species"]["url"]).text)["names"][4]["name"]
+                spriteUrl = pokemonJson["sprites"]["other"]["official-artwork"]["front_default"]
+                dataRender.append({"id": i + offset + 1, "name": nameFrench, "sprite": spriteUrl})
+
+        print("--- %s seconds END ---" % (time.time() - start_time))
+        return render(request, 'pokedex/accueil.html', {'data': dataRender, 'offset': offset, 'limit': limit})
+    else:
+        return redirect('accueil')
+
+
+async def getPokemonData(session, url):
+    async with session.get(url) as result:
+        dataPokemon = await result.json()
+        return dataPokemon
 
 
 def src_pokemon(request):
@@ -50,7 +86,6 @@ def src_pokemon(request):
             pokemon = response.text
             parse_json = json.loads(pokemon)
             id = parse_json["id"]
-            logging.info(id)
-            return HttpResponseRedirect(str(id))
-    else:
-        form = PokemonForm()
+            return redirect('index', str(id))
+        else:
+            return redirect('index', str(random.randint(1, 1117)))
