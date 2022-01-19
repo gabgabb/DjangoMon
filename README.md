cbc3ec6fc2e820ad9e8c7d59657818d106c8a2d5
Romain857 <romainmart.85@gmail.com>
2022-01-19 23:26:37 +0100

    create database team pokemon

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # DjangoMon
 
+# installer toutes les dépendances
+pip install -r requiements.txt
+
+# run server
+py manage.py runserver
+
+# créer la migration
+py manage.py makemigrations
+
+# migrer vers sa base
+py manage.py migrate
--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    pokemon1 = models.IntegerField(max_length=4, null=True)
+    pokemon2 = models.IntegerField(max_length=4, null=True)
+    pokemon3 = models.IntegerField(max_length=4, null=True)
+    pokemon4 = models.IntegerField(max_length=4, null=True)
+    pokemon5 = models.IntegerField(max_length=4, null=True)
+    pokemon6 = models.IntegerField(max_length=4, null=True)
+    
+def __str__(self):
+        return f"{self.pokemon1}:{self.pokemon2}:{self.pokemon3}:{self.pokemon4},{self.pokemon5},{self.pokemon6}"
