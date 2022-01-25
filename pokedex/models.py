f6a108f1c9a9351c46ce8d57d43ae8a9e88c0013
gabgabb <70889534+gabgabb@users.noreply.github.com>
2022-01-25 11:30:27 +0100

    supp max_lenght

--- pokedex/models.py
+++ pokedex/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Team(models.Model):
-    pokemon1 = models.IntegerField(max_length=4, null=True)
-    pokemon2 = models.IntegerField(max_length=4, null=True)
-    pokemon3 = models.IntegerField(max_length=4, null=True)
-    pokemon4 = models.IntegerField(max_length=4, null=True)
-    pokemon5 = models.IntegerField(max_length=4, null=True)
-    pokemon6 = models.IntegerField(max_length=4, null=True)
+    pokemon1 = models.IntegerField(null=True)
+    pokemon2 = models.IntegerField(null=True)
+    pokemon3 = models.IntegerField(null=True)
+    pokemon4 = models.IntegerField(null=True)
+    pokemon5 = models.IntegerField(null=True)
+    pokemon6 = models.IntegerField(null=True)
     
 def __str__(self):
         return f"{self.pokemon1}:{self.pokemon2}:{self.pokemon3}:{self.pokemon4},{self.pokemon5},{self.pokemon6}"
